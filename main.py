61a4802af3cb6f2e833d162499e1fd7a0be0b90a
Gabriel Martins <gbmmorais50@gmail.com>
2024-11-17 18:59:25 -0400

    ele roda, mas falta testar

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 class Operator:
-    def __init__(self,symbol,function,precedence):
+    def __init__(self,symbol,function,precedence,parameters):
         self.symbol=symbol;
         self.function=function;
         self.precedence=precedence;
+        self.parameters=parameters;
 
     def __str__(self):
-        return f"{self.symbol}, {self.function}, {self.precedence}";
+        return f"{self.symbol}, {self.function}, {self.precedence}, {self.parameters}";
+
 
 
 def func_and(*values):
@@ -17,6 +19,7 @@ def func_and(*values):
     return resultado
 
 
+
 def func_or(*values):
     resultado = False  
     for value in values:  
@@ -25,19 +28,17 @@ def func_or(*values):
             break
     return resultado
 
+def func_neg(*values):
+    return not values[0];
 
-def func_neg(value):
-    return not value;
-
-
-def func_cond(value1,value2):
-    if(value1==True and value2==False):
+def func_cond(*values):
+    if(values[0]==True and values[1]==False):
         return False;
     else:
         return True;
 
-def func_bicond(value1,value2):
-    if(value1==value2):
+def func_bicond(*values):
+    if(values[0]==values[1]):
         return True;
     else:
         return False;
@@ -45,14 +46,14 @@ def func_bicond(value1,value2):
 
 
 list_operators = [
-        Operator("(",None,0),
-        Operator(")",None,0),
+        Operator("(",None,0,0),
+        Operator(")",None,0,0),
         
-        Operator("~",func_neg,5),
-        Operator("^",func_and,4),
-        Operator("v",func_or,3),
-        Operator(">",func_cond,2),
-        Operator("-",func_bicond,1), 
+        Operator("~",func_neg,5,1),
+        Operator("^",func_and,4,2),
+        Operator("v",func_or,3,2),
+        Operator(">",func_cond,2,2),
+        Operator("-",func_bicond,1,2), 
 ]
 
 def buscaOperator(token):
@@ -79,12 +80,8 @@ for token in input_expression:
         elif(token=='B'):
             output.append(False);
         elif(token=='C'):
-            output.append(False);
-    elif ((operator.symbol != "(") and (operator.symbol != ")")):
-        while holding_stack[-1].precedence > operator.precedence:
-            output.append(holding_stack.pop());
-
-        holding_stack.append(operator);
+            output.append(True);
+    
     elif (operator.symbol == "("):
         holding_stack.append(operator);
     elif (operator.symbol == ")"):
@@ -94,9 +91,43 @@ for token in input_expression:
         holding_stack.pop();
         continue;
 
+    
+    elif (operator.symbol == "("):
+        holding_stack.append(operator);
+    
+    elif (operator.symbol == ")"):
+        while holding_stack[-1].symbol != "(":
+            output.append(holding_stack.pop());
+
+        holding_stack.pop();
+        continue;
+
+    elif(len(holding_stack)==0):
+        holding_stack.append(operator);
+
+    elif ((operator.symbol != "(") and (operator.symbol != ")")):
+        while holding_stack[-1].precedence > operator.precedence:
+            output.append(holding_stack.pop());
+
+        holding_stack.append(operator);
+    
+
 while len(holding_stack)>0:
     output.append(holding_stack.pop());
 
+for item in output:
+    if isinstance(item,bool):
+        solve_stack.append(item);
+    elif isinstance(item,Operator):
+        uni_resul=item.function(solve_stack[(-item.parameters):]);
+        for i in range(-1,-(item.parameters)):
+            solve_stack.pop();
+
+        solve_stack.append(uni_resul);
+
+final_resul=solve_stack[0];
+
+print(f"{final_resul}");
 
 
 
